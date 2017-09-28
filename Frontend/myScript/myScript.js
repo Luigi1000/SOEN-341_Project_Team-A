c4bb63b327c84b6f14bd0e72430eb3cc72169d8a
anbochuan <anbochuan@hotmail.com>
2017-09-28 17:29:44 -0400

    registration validation, add myScript.js file
    1.  email validation
    2. password validation
    3. re-type match
    4. username validation

---
+++ Frontend/myScript/myScript.js
@@ -0,0 +1,58 @@
+function registerValidation()
+{
+	var e = true;
+	var email = document.getElementById('inputEmail');
+	var patn = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var m = email.value.search(patn);
+
+	if(m == -1)
+	{
+		alert("The email format is not correct! Please enter your email again!");
+		e = false;
+	}
+	else
+		e = true;
+
+	var s = true;
+	var pwd = document.getElementById("pwd");
+	var b = pwd.value.search(/^[a-zA-Z0-9]{8,}$/);
+
+	if(b == -1 || pwd.length < 8)
+	{
+		alert("The password format is not correct! Please enter your password again!");
+		s = false;
+	}
+	else
+		s = true;
+
+	var r = true;
+	var pwd = document.getElementById("pwd");
+	var repwd = document.getElementById("repwd");
+
+	if(pwd.value != repwd.value )
+	{
+		alert("The Re-type Password doed not match the initial one! Please enter your password again!");
+		r = false;
+	}
+	else
+		r = true;
+
+	var v = true;
+	var patn = document.getElementById("username");
+	var a = patn.value.search(/[a-zA-Z0-9]/);
+
+	if(a == -1) 
+	{
+		alert("The nickname format is not correct! Please enter your nickname again!");
+		v = false;
+	}
+	else
+		v = true;
+
+	if(e==true && s==true && r==true && v==true) 
+	{
+		return true;
+	}
+	else
+		return false;
+}
