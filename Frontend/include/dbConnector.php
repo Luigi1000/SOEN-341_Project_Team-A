d020eaa45061f00d1af39f76bb92d04b86471c5b
Luigi1000 <Wise_guy_92@hotmail.com>
2017-09-30 11:04:27 -0400

    Data base connection for init. and listing page
    Changed the connection script to connect to database as well as touched
    up listings page to recursivly pull database entries for the listings
    page.

--- Frontend/include/dbConnector.php
+++ Frontend/include/dbConnector.php
@@ -1,15 +1,8 @@
   <?php
       $servername = 'localhost';
-      $username = '';
-      $dbname = '';
+      $username = "root";
+      $dbname = 'soen341';
       $password = '';
 
-      // make connection   (connecting to where + user name + password + DBname)
-      $conn = new mysqli($servername, $username, $password, $dbname);
-
-        // Check connection
-      if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-      } 
-      echo "Database Connection Succeed!";
+	  $db = new PDO("mysql:host=$servername;dbname=soen341;charset=utf8", "$username", "$password", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
     ?>
