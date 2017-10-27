a673421a60b2207b255ea9f960ea7f476e33fa32
Luigi1000 <Wise_guy_92@hotmail.com>
2017-10-27 10:24:25 -0400

    Adation of two test cases.
    Addation of two test cases for travis CI for phpunit. One which just
    checks the sum of a value where the other one checks a user logging in.

---
+++ unittests/AddTest.php
@@ -0,0 +1,27 @@
+<?php 
+
+	use PHPUnit\Framework\TestCase;
+	
+	class AddTest extends TestCase 
+	{
+		/**
+		 * @dataProvider mathValues
+		 */
+		public function testAdd($x, $y, $sum) 
+		{
+			$this->assertEquals($sum, $x+$y);
+		}
+		
+		public function mathValues
+		{
+			return[
+				[0,0,0],
+				[1,1,2],
+				[2,3,5],
+				[6,6,12],
+				[8,8,16],
+				[25,25,50]
+			];
+		}
+	}
+?>
