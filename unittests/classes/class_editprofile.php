0d2c1967ec906bf07e2b7e0361a80bc4838f0c39
nechawla <31772617+nechawla@users.noreply.github.com>
2017-11-01 23:08:52 -0400

    #127  Unit test

---
+++ unittests/classes/class_editprofile.php
@@ -0,0 +1,31 @@
+<?php 
+<?php
+class EditProfile 
+	{ 
+ 		 
+ 		public function __construct() 
+ 		{ 
+ 	        
+ 	    } 
+ 		 
+ 		public function editProfile($userName, $address, $phoneNumber) 
+ 		{ 
+ 			 
+ 			$serverName = '108.63.178.195'; 
+ 			$userName = "root"; 
+			$dbName = 'soen341'; 
+ 			$passWord = ''; 
+  
+ 			$db = new PDO("mysql:host=$serverName;port=3306;dbname=soen341;charset=utf8", "$userName", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
+ 			$log = $db->query("SELECT * FROM `user` WHERE `UserName` = '$userName'"); 
+			$profile = $log->fetch(\PDO::FETCH_ASSOC); 
+ 			
+ 			 
+ 			if ($userName == $profile ['UserName'] && $address == $profile['Address'] && $phoneNumber == $profile ['PhoneNumber']) 
+ 				return true; 
+ 			else 
+				return false; 
+ 		} 
+ 		 
+ 	} 
+ ?> 
