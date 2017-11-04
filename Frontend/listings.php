ac31006ac5bd653b49128bf477a380e76ddcdd78
anbochuan <anbochuan@hotmail.com>
2017-11-03 22:50:37 -0400

    when three layers of the categories have been clicked, user can sort by price from highest to lowest order

--- Frontend/listings.php
+++ Frontend/listings.php
@@ -295,6 +295,10 @@
               {
                 $resultArray = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductCategory1 = '{$_GET['category']}' AND ProductCategory2= '{$_GET['subcategory']}' AND ProductCategory3= '{$_GET['ssubcategory']}' ORDER BY timeStamp DESC");
               }
+              if($sortingRequirement == 'priceDesc')
+              {
+                $resultArray = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductCategory1 = '{$_GET['category']}' AND ProductCategory2= '{$_GET['subcategory']}' AND ProductCategory3= '{$_GET['ssubcategory']}' ORDER BY Price DESC");
+              }
               
             }
             else // if user do not want to sort, just randomly display everyting order by ProductId
