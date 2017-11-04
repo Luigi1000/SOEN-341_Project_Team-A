abc32848ac2009341bbc87fd783c2a3abd8e8a92
anbochuan <anbochuan@hotmail.com>
2017-11-03 21:43:08 -0400

    change the search bar form method to 'get' and add new city and province for search bar dropdown menu
    1. changed the search bar form method to ‘get’ for sortBy feature
    implementation later on.
    2. add more city and province on the search bar dropdown menu in order
    to keep testing the search bar function.

--- Frontend/include/header.php
+++ Frontend/include/header.php
@@ -151,7 +151,7 @@
 
   <!-- search bar -->
   <div class="well text-center">
-    <form class="form-inline" action="listings.php" method="post">
+    <form class="form-inline" action="listings.php" method="get">
       <div class="form-group">
         <input type="text" class="form-control" placeholder="Search for anything..." name="item" size="65">
       </div>
@@ -172,25 +172,26 @@
         <!-- select dropdown menu -->
           <select class="selectpicker form-control" name="city" required>
               <option value="" selected disabled>Select location...</option>
-            <!-- <optgroup label="Alberta">
-              <option value="Banff/Canmore">Banff/Canmore</option>
-              <option value="Edmonton Area">Edmonton Area</option>
-              <option value="Fort McMurray">Fort McMurray</option>
+            <optgroup label="Alberta">
+              <option value="Alberta">All of Alberta</option>
+              <option value="BanffCanmore">Banff/Canmore</option>
+              <option value="EdmontonArea">Edmonton Area</option>
+              <option value="FortMcMurray">Fort McMurray</option>
             </optgroup>
             <optgroup label="British Columbia">
-              <option value="Cariboo Area">Cariboo Area</option>
-              <option value="Comox Valley Area">Comox Valley Area</option>
-              <option value="Cowicha Valley">Cowicha Valley</option>
+              <option value="BritishColumbia">All of British Columbia</option>
+              <option value="CaribooArea">Cariboo Area</option>
+              <option value="CowichaValley">Cowicha Valley</option>
             </optgroup>
             <optgroup label="Ontario">
+              <option value="Ontario">All of Ontario</option>
               <option value="Muskoka">Muskoka</option>
-              <option value="North Bay">North Bay</option>
+              <option value="NorthBay">North Bay</option>
               <option value="Toronto">Toronto</option>
-            </optgroup> -->
+            </optgroup>
             <optgroup label="Quebec">
-              <!-- <option value="Abitibi-Temiscamingue">Abitibi-Temiscamingue</option>
-              <option value="Granby">Granby</option> -->
               <option value="Quebec">All of Quebec</option>
+              <option value="Laval">Laval</option>
               <option value="QuebecCity">Quebec City</option>
               <option value="Laval">Laval</option>
               <option value="Montreal">Montreal</option>
