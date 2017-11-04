9dba9ae5777271012e229c3773c655e344ea06a3
anbochuan <anbochuan@hotmail.com>
2017-11-03 22:48:45 -0400

    when three layers of the categories have been clicked, user can sort by publish date from newest to oldest order

--- Frontend/listings.php
+++ Frontend/listings.php
@@ -291,6 +291,10 @@
               {
                 $resultArray = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductCategory1 = '{$_GET['category']}' AND ProductCategory2= '{$_GET['subcategory']}' AND ProductCategory3= '{$_GET['ssubcategory']}' ORDER BY timeStamp ASC");
               }
+              if($sortingRequirement == 'dateDesc')
+              {
+                $resultArray = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductCategory1 = '{$_GET['category']}' AND ProductCategory2= '{$_GET['subcategory']}' AND ProductCategory3= '{$_GET['ssubcategory']}' ORDER BY timeStamp DESC");
+              }
               
             }
             else // if user do not want to sort, just randomly display everyting order by ProductId
