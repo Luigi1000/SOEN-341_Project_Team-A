f0be43e6c0745cbc5eee4b16d610aa18c6924fe5
Luigi1000 <Wise_guy_92@hotmail.com>
2017-11-14 15:28:51 -0500

    updating the mysql commands due to mismatch between database entry and columns.

--- travis.sql
+++ travis.sql
@@ -7,8 +7,21 @@ CREATE DATABASE IF NOT EXISTS `soen341` DEFAULT CHARACTER SET utf8 COLLATE utf8_
 USE `soen341`;
 
 # Create Table
-CREATE TABLE IF NOT EXISTS `user` (UserId int(11) NOT NULL AUTO_INCREMENT, UserName varchar(45) NOT NULL, Email varchar(45) NOT NULL, Password varchar(45) NOT NULL, Address varchar(45) DEFAULT NULL, CityName varchar(45) DEFAULT NULL, Province varchar(45) DEFAULT NULL, Country varchar(45) DEFAULT NULL, PRIMARY KEY (UserId), UNIQUE KEY Email_UNIQUE (Email), UNIQUE KEY UserName_UNIQUE (UserName));
-
+CREATE TABLE IF NOT EXISTS `user` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
+  `UserName` varchar(45) NOT NULL,
+  `Email` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Address` varchar(45) DEFAULT NULL,
+  `CityName` varchar(45) DEFAULT NULL,
+  `Province` varchar(45) DEFAULT NULL,
+  `Country` varchar(45) DEFAULT NULL,
+  `image` longblob,
+  `PhoneNumber` int(11) DEFAULT NULL,
+  PRIMARY KEY (`UserId`),
+  UNIQUE KEY `Email_UNIQUE` (`Email`),
+  UNIQUE KEY `UserName_UNIQUE` (`UserName`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 # Add Data
 INSERT IGNORE INTO `user`(`UserId`, `UserName`, `Email`, `Password`, `Address`, `CityName`, `Province`, `Country`, `image`, `PhoneNumber`) VALUES (Null,'yougtang','wilsonluyongtang@gmail.com','49ba742a6151092935053e64da7deb3e',Null,Null,Null,Null,Null,Null);
