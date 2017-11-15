41566feb69dfcee26dc7b0f5523a34ef78f0a328
Yang8547 <anyang47@hotmail.com>
2017-11-15 12:39:06 -0500

    correct syntax error

--- unittests/EditProfileTest.php
+++ unittests/EditProfileTest.php
@@ -12,7 +12,7 @@
  			 
  			$userName = 'neel'; 
  			$address = '123 St catherine'; 
-			$phoneNumber = '5146541232'
+			$phoneNumber = '5146541232';
  			$expect = true; 
  			$lg = new EditProfile(); 
  			$result = $lg->editProfile($userName, $address, $phoneNumber); 
