679631fa42c54e85a88acb17568b4ebf0addb1ba
Dania Kalomiris <daniakalomiris@gmail.com>
2017-11-16 17:32:59 -0500

    Update class_itemlisting.php
    Remove if-else statement

--- unittests/classes/class_itemlisting.php
+++ unittests/classes/class_itemlisting.php
@@ -16,16 +16,8 @@
  			$dbname = 'soen341'; 
   			$passWord = ''; 
   			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$result = $db->("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId");
-			
-            		if ($itemNumber == $result['ProductId'] && $itemName == $result['ProductName'] && $itemPrice == $result['Price'] && $itemDescription == $result['ProductDetail'] && $userId == $result['UserId'])  
-            		{
-            			return true;
-           		}
-            		else 
-            		{
-            			return false;
-            		}
+			$result = $db->("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductId = '$itemNumber' AND ProductName = '$itemName' AND Price = '$itemPrice' AND ProductDetail = '$itemDescription' AND UserId = '$userId'");
+			return $result;
 		}
 	}
 ?>
