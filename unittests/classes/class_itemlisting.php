db400229c4df41531fc720811fde07793aa20c06
Dania Kalomiris <daniakalomiris@gmail.com>
2017-11-16 17:09:01 -0500

    Update class_itemlisting.php

--- unittests/classes/class_itemlisting.php
+++ unittests/classes/class_itemlisting.php
@@ -16,9 +16,9 @@
  			$dbname = 'soen341'; 
   			$passWord = ''; 
   			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$resultArray = $db->("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId");
+			$result = $db->("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId");
 			
-            		if ($itemNumber == $listing['ProductId'] && $itemName == $listing['ProductName'] && $itemPrice == $listing['Price'] && $itemDescription == $listing['ProductDetail'] && $userId == $listing['UserId'])  
+            		if ($itemNumber == $result['ProductId'] && $itemName == $result['ProductName'] && $itemPrice == $result['Price'] && $itemDescription == $result['ProductDetail'] && $userId == $result['UserId'])  
             		{
             			return true;
            		}
