2552cce7956e488cdf635974c0a0905d32177dfd
Dania Kalomiris <daniakalomiris@gmail.com>
2017-11-16 17:05:52 -0500

    Update class_itemlisting.php

--- unittests/classes/class_itemlisting.php
+++ unittests/classes/class_itemlisting.php
@@ -15,20 +15,17 @@
  			$username = "root"; 
  			$dbname = 'soen341'; 
   			$passWord = ''; 
-  			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
-            $loc = $db->query("SELECT * FROM product WHERE ProductId = '$itemNumber'");
-            $listing = $loc->fetch(\PDO::FETCH_ASSOC);
-
-            if ($itemNumber == $listing['ProductId'] && $itemName == $listing['ProductName'] && $itemPrice == $listing['Price'] && $itemDescription == $listing['ProductDetail'] && $userId == $listing['UserId'])  
-            {
-            	return true;
-            }
-            else 
-            {
-            	return false;
-            }
-		    
+  			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+			$resultArray = $db->("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId");
+			
+            		if ($itemNumber == $listing['ProductId'] && $itemName == $listing['ProductName'] && $itemPrice == $listing['Price'] && $itemDescription == $listing['ProductDetail'] && $userId == $listing['UserId'])  
+            		{
+            			return true;
+           		}
+            		else 
+            		{
+            			return false;
+            		}
 		}
 	}
-
 ?>
