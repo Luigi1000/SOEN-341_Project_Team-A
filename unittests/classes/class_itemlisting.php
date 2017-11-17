ac5e585874bda2064bdaeab0245809ad4c5a6acb
Luigi1000 <Wise_guy_92@hotmail.com>
2017-11-16 21:47:00 -0500

    Correction of return type for itemlistings unit test class

--- unittests/classes/class_itemlisting.php
+++ unittests/classes/class_itemlisting.php
@@ -16,7 +16,8 @@
  			$dbname = 'soen341'; 
   			$passWord = ''; 
   			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$result = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductId = '$itemNumber' AND ProductName = '$itemName' AND Price = '$itemPrice' AND ProductDetail = '$itemDescription'");
+			$log = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductId = '$itemNumber' AND ProductName = '$itemName' AND Price = '$itemPrice' AND ProductDetail = '$itemDescription'");
+			$result = $log->fetch(\PDO::FETCH_ASSOC); 
 			
 			return $result;
 		}
