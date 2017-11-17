4b9cde2a19e8f355ff358cda1f1ce6500e695a9f
Luigi1000 <Wise_guy_92@hotmail.com>
2017-11-16 21:22:20 -0500

    Correction of query sql syntax for listings unit test

--- unittests/ItemListingTest.php
+++ unittests/ItemListingTest.php
@@ -14,7 +14,6 @@
 			$expectedItemName = 'Pug Dog';
 			$expectedItemPrice = '500';
 			$expectedItemDescription = 'Available in black and brown';
-			$expectedUserId = '2'; 
 
 			$clickItem = new ItemListing;
 			$click = $clickItem->item($expectedItemNumber, $expectedItemName, $expectedItemPrice, $expectedItemDescription, $expectedUserId); 
@@ -22,7 +21,6 @@
 			$this->assertEquals($expectedItemName, $click['ProductName']);	
 			$this->assertEquals($expectedItemPrice, $click['Price']);	
 			$this->assertEquals($expectedItemDescription, $click['ProductDetail']);	
-			$this->assertEquals($expectedUserId, $click['UserId']);	
 		}
 	}
 
--- unittests/classes/class_itemlisting.php
+++ unittests/classes/class_itemlisting.php
@@ -16,7 +16,7 @@
  			$dbname = 'soen341'; 
   			$passWord = ''; 
   			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-			$result = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductId = '$itemNumber' AND ProductName = '$itemName' AND Price = '$itemPrice' AND ProductDetail = '$itemDescription' AND UserId = '$userId'");
+			$result = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductId = '$itemNumber' AND ProductName = '$itemName' AND Price = '$itemPrice' AND ProductDetail = '$itemDescription'");
 			return $result;
 		}
 	}
