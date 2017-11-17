9296fe8943cd41ebc1b6eb1427a18c57726ab723
Luigi1000 <Wise_guy_92@hotmail.com>
2017-11-16 21:30:05 -0500

    Correction of syntax for the class of the itemlistings unit test.

--- unittests/classes/class_itemlisting.php
+++ unittests/classes/class_itemlisting.php
@@ -17,6 +17,7 @@
   			$passWord = ''; 
   			$db = new PDO("mysql:host=$servername;port=3306;dbname=soen341;charset=utf8", "$username", "$passWord", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 			$result = $db->query("SELECT * FROM product INNER JOIN user ON product.UserId = user.UserId WHERE ProductId = '$itemNumber' AND ProductName = '$itemName' AND Price = '$itemPrice' AND ProductDetail = '$itemDescription'");
+			
 			return $result;
 		}
 	}
