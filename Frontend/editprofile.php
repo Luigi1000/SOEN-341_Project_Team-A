7f1ad8e26e7cbc3a9a906e2aab35e0e19bf5f12c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-19 07:59:04 +0800

    Delete Frontend directory

--- Frontend/editprofile.php
+++
@@ -1,3 +0,0 @@
-$sql = "SELECT * FROM user where user_username='$user_username'";
-    $result = mysqli_query($database,$sql) or die(mysqli_error($database)); 
-    $rws = mysqli_fetch_array($result);
--- Frontend/include/dbConnector2.php
+++
@@ -1,8 +0,0 @@
-  <?php
-      $servername = 'localhost';
-      $username = "root";
-      $dbname = 'soen341';
-      $password = '';
-
-	  $db = new PDO("mysql:host=$servername;dbname=soen341;charset=utf8", "$username", "$password", array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)); 
-    ?>
